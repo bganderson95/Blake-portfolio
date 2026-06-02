ff0996f51b2a6dd065db22737a726346a8530c68
bganderson95 <bganderson95@gmail.com>
2026-06-02 09:29:34 -0400

    updated nav link and logo

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -1,7 +1,10 @@
+import { useEffect } from 'react'
 import { About } from '../components/About'
 import { Footer } from '../components/Footer'
 
 export function AboutPage() {
+  useEffect(() => { window.scrollTo(0, 0) }, [])
+
   return (
     <>
       <main className="about-page">
